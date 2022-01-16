07fe47059e0eca8813c743d0bfa157671bc6ec63
unknown <muhammad.barcot@gmail.com>
2022-01-16 22:57:33 +0600

    Files updated

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	
+	/*==========OwlCarousel=================
+	N:B: In custom/main.js files .owl-carousel change With .slider-active(or your slider activation class name)
+		& this .owl-carousel class must be added in your html slide-item  to work Carousel.
+	$('.owl-carousel').owlCarousel({
+	  items: 1,
+	  autoplay: true,
+	  loop: true,
+	  nav: false,
+	  dots: true
+	 });
+	 
+	 ---------------------------------------*/
+	 
+});
+
+	/*==========Using Magnific POpUp=================*/
+
+jQuery(document).ready(function($){
+	$(".video-play-btn").magnificPopup({
+		type: 'video'
+	});
+});
+
